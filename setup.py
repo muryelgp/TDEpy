c5cec76d2f28c3ead5949e7f9596485567c12d50
muryelgp <muryel@astro.ufsc.br>
2021-05-12 01:03:57 -0300

    Change package strucutre, add setup.py and add new filters

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TDEpy',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='muryel',
+    author_email='muryel@astro.ufsc.br',
+    description=''
+)
